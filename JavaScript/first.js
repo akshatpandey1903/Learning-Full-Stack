0d57ef386a7c474d7fdd6c1e58b87b4a413d5882
Akshat Pandey <akshatp1903@gmail.com>
2024-08-24 23:50:22 +0530

    Starting with JS

---
+++ JavaScript/first.js
@@ -0,0 +1,19 @@
+console.log("Starting my journey");
+console.log("Akshat Pandey");
+x = null;
+y = undefined;
+console.log(x);
+console.log(y);
+
+let name = "Tony";
+console.log(name);
+
+const PI = 3.14;
+console.log(PI);
+
+const student = {
+    fullName : "Rahul",
+    age : 20,
+    cgpa : 8.2,
+    isPass : true
+}
